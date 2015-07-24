341d1390788b2c797d2dff8fe1707c113899fa41
Tyler Wong <TylerWong@SEEs-MacBook-Pro.local>
2015-07-24 12:17:43 -0700

    added tree class

---
+++ bptree/bptree.cpp
@@ -0,0 +1,21 @@
+//
+//  bptree.cpp
+//  bptree
+//
+//  Created by Tyler Wong on 2015-07-24.
+//  Copyright (c) 2015 Tyler Wong. All rights reserved.
+//
+
+#include "bptree.h"
+bptree::bptree()
+{
+    //implementation here
+}
+
+int bptree::insert(int parameters)
+{
+    holder = parameters;
+    printf("holder value is %d \n", holder);
+    //implemenation here
+    return 0;
+}
---
+++ bptree/bptree.h
@@ -0,0 +1,29 @@
+//
+//  bptree.h
+//  bptree
+//
+//  Created by Tyler Wong on 2015-07-24.
+//  Copyright (c) 2015 Tyler Wong. All rights reserved.
+//
+
+#ifndef __bptree__bptree__
+#define __bptree__bptree__
+
+#include <stdio.h>
+
+class bptree
+{
+public:
+    bptree();
+    
+    int holder;
+    int insert(int parameters);
+    int remove(int parameters);
+    int find(int parameters);
+    int printkeys(int parameters);
+    int printvalues(int parameters);
+    
+};
+
+
+#endif /* defined(__bptree__bptree__) */
